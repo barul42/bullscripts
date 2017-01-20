bb9738b6d9ff6433e92db3bf7526948444c1a6de
barul <barul+git@barul.info>
2017-01-20 07:43:07 +0100

    updated abi_x86_32

--- abi_x86_32.sh
+++ abi_x86_32.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#for a in $(cut -d'/' -f2 test2| sort); do touch ./test3/$a; done
-a="$(cut -d'/' -f2 ./files/test2)"
-for i in $(cat ./files/test2); do echo "${i} abi_x86_32" >> "./files/test3/${a}"; done
+#a="$(cut -d'/' -f2 ./files/test2)"
+#for a in $(cut -d'/' -f2 ./files/test2); do touch ./files/test3/${a}; done
+#echo $a
+#for i in $(cat ./files/test2); do echo "${i} abi_x86_32" i>> "./files/test3/${a}"; done
+for i in $(cat ./files/test2); do echo "${i} abi_x86_32" >> "./files/test3/$(cut -d'/' -f2 ./files/test2)"; done
